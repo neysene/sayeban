89a8909a5bbb95c0147731f36e18b7318d847998
GofS <ismailgonul@gmail.com>
2019-05-11 22:32:13 +0300

    Test to push sth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AddTipComponent } from './add-tip/add-tip.component';
 
 import { AdminGuard } from './core/admin.guard';
 import { CanReadGuard } from './core/can-read.guard';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [CanReadGuard] },
-  { path: 'admin', component: AdminPanelComponent, canActivate: [AdminGuard] }
+  { path: 'admin', component: AdminPanelComponent, canActivate: [AdminGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'add-tip', component: AddTipComponent, canActivate: [AdminGuard] }
 ];
 
 @NgModule({
